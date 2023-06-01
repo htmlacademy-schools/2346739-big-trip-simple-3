3445949187f0ca33bfa6e442705205bfcc5d7072
MeeLeSh <teadorWOT@gmail.com>
2023-06-01 19:59:03 +0300

    module8-task2

--- src/presenter/trip-point-presenter.js
+++ src/presenter/trip-point-presenter.js
@@ -63,7 +63,8 @@ export default class TripPointPresenter {
         replace(this.#tripPointComponent, prevTripPointComponent);
         break;
       case Mode.EDITING:
-        replace(this.#editFormComponent, prevEditFormComponent);
+        replace(this.#tripPointComponent, prevEditFormComponent);
+        this.#mode = Mode.DEFAULT;
     }
 
     remove(prevEditFormComponent);
@@ -79,6 +80,24 @@ export default class TripPointPresenter {
     if (this.#mode !== Mode.DEFAULT) {
       this.#editFormComponent.reset(this.#tripPoint);
       this.#replaceFormToPoint();
+    }
+  }
+
+  setSaving() {
+    if (this.#mode === Mode.EDITING) {
+      this.#editFormComponent.updateElement({
+        isDisabled: true,
+        isSaving: true,
+      });
+    }
+  }
+
+  setDeleting() {
+    if (this.#mode === Mode.EDITING) {
+      this.#editFormComponent.updateElement({
+        isDisabled: true,
+        isDeleting: true,
+      });
     }
   }
 
@@ -109,6 +128,23 @@ export default class TripPointPresenter {
     document.body.addEventListener('keydown', this.#ecsKeyDownHandler);
   };
 
+  setAborting() {
+    if (this.#mode === Mode.DEFAULT) {
+      this.#tripPointComponent.shake();
+      return;
+    }
+
+    const resetFormState = () => {
+      this.#editFormComponent.updateElement({
+        isDisabled: false,
+        isSaving: false,
+        isDeleting: false,
+      });
+    };
+
+    this.#editFormComponent.shake(resetFormState);
+  }
+
   #handleFormSubmit = (update) => {
     const isMinorUpdate = !isDatesEqual(this.#tripPoint.dateFrom, update.dateFrom) || this.#tripPoint.basePrice !== update.basePrice;
     this.#handleDataChange(
@@ -116,7 +152,6 @@ export default class TripPointPresenter {
       isMinorUpdate ? UpdateType.MINOR : UpdateType.PATCH,
       update,
     );
-    this.#replaceFormToPoint();
     document.body.removeEventListener('keydown', this.#ecsKeyDownHandler);
   };
 
