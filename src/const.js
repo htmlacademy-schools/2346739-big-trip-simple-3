dd0f7f928bfa334a3b33ae6c27924d0efddf7a8e
MeeLeSh <teadorWOT@gmail.com>
2023-06-05 16:39:03 +0300

    fix eoc

--- src/const.js
+++ src/const.js
@@ -3,13 +3,13 @@ const FilterType = {
   FUTURE: 'future',
   PAST: 'past'
 };
-  
+
 const FilterTypeDescriptions = {
   [FilterType.EVERYTHING]: 'EVERYTHING',
   [FilterType.PAST]: 'PAST',
   [FilterType.FUTURE]: 'FUTURE',
 };
-  
+
 const SortType = {
   DAY: 'day',
   EVENT: 'event',
@@ -17,7 +17,7 @@ const SortType = {
   PRICE: 'price',
   OFFER: 'offer'
 };
-  
+
 const SortTypeDescription = {
   [SortType.DAY]: 'Day',
   [SortType.EVENT]: 'Event',
@@ -25,20 +25,20 @@ const SortTypeDescription = {
   [SortType.PRICE]: 'Price',
   [SortType.OFFER]: 'Offer'
 };
-  
+
 const UserAction = {
   UPDATE_TRIPPOINT: 'UPDATE_TRIPPOINT',
   ADD_TRIPPOINT: 'ADD_TRIPPOINT',
   DELETE_TRIPPOINT: 'DELETE_TRIPPOINT',
 };
-  
+
 const UpdateType = {
   PATCH: 'PATCH',
   MINOR: 'MINOR',
   MAJOR: 'MAJOR',
   INIT: 'INIT',
 };
-  
+
 const pointTypes = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
-  
+
 export {SortType, FilterTypeDescriptions, SortTypeDescription, UserAction, UpdateType, FilterType, pointTypes};
