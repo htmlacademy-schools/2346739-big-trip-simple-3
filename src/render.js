500f22cc35ac29c491a5fb3d83aefdc17c7a4854
MeeLeSh <teadorWOT@gmail.com>
2023-06-05 16:45:39 +0300

    fix eoc

--- src/render.js
+++ src/render.js
@@ -4,16 +4,16 @@ const RenderPosition = {
   BEFOREEND: 'beforeend',
   AFTEREND: 'afterend',
 };
-  
+
 function createElement(template) {
   const newElement = document.createElement('div');
   newElement.innerHTML = template;
-  
+
   return newElement.firstElementChild;
 }
-  
+
 function render(component, container, place = RenderPosition.BEFOREEND) {
   container.insertAdjacentElement(place, component.element);
 }
-  
+
 export {RenderPosition, createElement, render};
